dadfe7c8b46ab35f2e046e1c012b34ad404b95a4
Rahmanda Trinova Putra <72428816+Trnvv11@users.noreply.github.com>
2023-07-05 17:04:17 +0700

    Add files via upload

---
+++ admin/kategori-detail.php
@@ -0,0 +1,41 @@
+<?php
+    require "session.php";
+    require "../koneksi.php";
+
+    $id = $_GET['e'];
+
+    $query = mysqli_query($con, "SELECT * FROM kategori WHERE id='$id'");
+    $data = mysqli_fetch_array($query);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detail Kategori</title>
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- Link Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+</head>
+<body>
+    <?php require "navbar.php";?>
+
+    <div class="container">
+    <h2>Detail Kategori</h2>
+
+        <div>
+            <form action="" method="post">
+                <label for="kategori">Kategori</label>
+                <input type="text" name="kategori" id="kategori" class="form-control" value="<?php echo $data['nama']; ?>">
+            </form>
+        </div>
+    </div>
+    
+    <!-- bundle -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js"></script>
+</body>
+</html>
